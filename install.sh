f5ba1f706150d38f563cbc0bdb5f846dc45962f6
Yaroslav Voropaev <128208855+zabojeb@users.noreply.github.com>
2024-08-16 22:56:07 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git clone https://github.com/zabojeb/pomotime.git pomotime
+cd pomotime
+
+sudo mv pomotime /usr/local/bin/pomotime
+sudo chmod +x /usr/local/bin/pomotime
+
+mkdir -p ~/.config/pomotime
+mv sounds ~/.config/pomotime/sounds
+mv pomotime.toml ~/.config/pomotime/pomotime.toml
+mv LICENSE ~/.config/pomotime/LICENSE
+mv README.md ~/.config/pomotime/README.md
+
+rm -rf .git .gitignore
+
+cd ..
+rmdir pomotime
+
+sudo sed -i 's/\r//' /usr/local/bin/pomotime
+sudo sed -i 's/\r//' ~/.config/pomotime/pomotime.toml
+
+echo "Pomotime installed succesfully!"
